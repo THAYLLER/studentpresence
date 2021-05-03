7dabb60ea27beb0bf4b84a0cbf7b4828f1b5acdf
thayller vilela cintra <thayllervilelacintra@yahoo.com.br>
2021-05-03 19:14:18 -0300

    up project

--- app/src/main/java/com/devnow/student/presence/MainActivity.kt
+++ app/src/main/java/com/devnow/student/presence/MainActivity.kt
@@ -1,34 +1,30 @@
 package com.devnow.student.presence
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.location.LocationManager
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
-import com.google.gson.Gson
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 private var locationManager : LocationManager? = null
 
 private val locationPermissionCode = 2
 
-private var longitude: Double = 0.0
-private var latitude: Double = 0.0
-
+val dist = FloatArray(1)
 val data = Date()
 var subject = ""
 
@@ -40,11 +36,16 @@ class MainActivity : AppCompatActivity() {
 
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode)
         }
-        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+            0L,
+            0f,
+            locationListener)
 
-
+        var off = 1
         val formataData = SimpleDateFormat("dd/MM/yyyy")
 
         val dataFormatada: String = formataData.format(data)
@@ -53,12 +54,9 @@ class MainActivity : AppCompatActivity() {
         val txt: TextView = findViewById(R.id.textSubject) as TextView
         val txtDate: TextView = findViewById(R.id.textDate) as TextView
         val txtInfo: TextView = findViewById(R.id.textInfo) as TextView
-        val txtLa: TextView = findViewById(R.id.textLocationLatitude) as TextView
-        val txtLo: TextView = findViewById(R.id.textLocationLongitude) as TextView
         val btn: Button = findViewById(R.id.button) as Button;
 
         subject = "NENHUMA AULA NESSE DIA"
-        var off = 1;
 
         when (this.getWeek(dataFormatada.toString())) {
             "SEG" -> subject = "Aula do dia: LINGUAGENS FORMAIS E AUTÔNOMAS"
@@ -69,13 +67,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        if(txtLa.text == "-23.536286105990403" && txtLo.text == "-46.560337171952156") {
-            off = 1;
+        if(dist[0].toDouble() == (1000000).toDouble()) {
+            if (dist[0] / 1000 > 1) {
+
+                off = 1;
+            }else {
+                off = 0;
+            }
         }else {
             off = 0;
         }
 
-        off = 1;
         if(off == 0) {
             btn.isInvisible = true;
             txtInfo.setText("Você não pode registrar presença hoje volte outro dia")
@@ -88,11 +90,14 @@ class MainActivity : AppCompatActivity() {
 
     private val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
-            val txtLa: TextView = findViewById(R.id.textLocationLatitude) as TextView
-            val txtLo: TextView = findViewById(R.id.textLocationLongitude) as TextView
 
-            txtLo.setText(location.longitude.toString())
-            txtLa.setText(location.latitude.toString())
+
+            Location.distanceBetween(
+                location.latitude,
+                location.longitude,
+                -23.536286105990403,
+                -46.560337171952156,
+                dist)
         }
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
         override fun onProviderEnabled(provider: String) {}
